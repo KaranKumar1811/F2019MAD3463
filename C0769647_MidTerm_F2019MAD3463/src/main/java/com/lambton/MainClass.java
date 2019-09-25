7b0d4d341ad3447bfc8c48064a8dc65a89bf5950
Karan Kumar <kumarkay.ca@gmail.com>
2019-09-25 18:34:59 -0400

    Calling Reverse String Function in main Class

--- C0769647_MidTerm_F2019MAD3463/src/main/java/com/lambton/MainClass.java
+++ C0769647_MidTerm_F2019MAD3463/src/main/java/com/lambton/MainClass.java
@@ -3,6 +3,9 @@ package com.lambton;
 public class MainClass {
     public static void main(String[] args) {
 
+        String a="Lambton";
+        LambtonStringTools obj1=new LambtonStringTools();
+        System.out.println(obj1.reverse(a));
 
 
     }
