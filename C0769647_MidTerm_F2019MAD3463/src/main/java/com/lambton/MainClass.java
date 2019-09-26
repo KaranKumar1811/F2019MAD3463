8a3904825c1945875e4f70ad49f629ef8e939397
Karan Kumar <kumarkay.ca@gmail.com>
2019-09-25 20:11:20 -0400

    main modify

--- C0769647_MidTerm_F2019MAD3463/src/main/java/com/lambton/MainClass.java
+++ C0769647_MidTerm_F2019MAD3463/src/main/java/com/lambton/MainClass.java
@@ -2,15 +2,12 @@ package com.lambton;
 
 public class MainClass {
     public static void main(String[] args) {
-
         String a="Lambton";
 
         LambtonStringTools obj1=new LambtonStringTools();
-
         System.out.println(obj1.reverse(a));
 
         String name="KaDFran kmsr kUMadAR";
-
         System.out.println(obj1.initials(name));
 
         String word="karan kumar";
