b2b77019e74f43fa0ace901aa6f720e61972039b
Karan Kumar <kumarkay.ca@gmail.com>
2019-09-25 20:07:25 -0400

    str rev

--- C0769647_MidTerm_F2019MAD3463/src/main/java/com/lambton/LambtonStringTools.java
+++ C0769647_MidTerm_F2019MAD3463/src/main/java/com/lambton/LambtonStringTools.java
@@ -2,13 +2,12 @@ package com.lambton;
 
 public class LambtonStringTools {
     public static String reverse(String s)
-    {
-        String rev="";
+    {        String rev="";
         for(int i=s.length()-1;i>=0;i--)
         {
             rev=rev+s.charAt(i);
         }
-    return rev;
+        return rev;
     }
 
     public static String initials(String s)
