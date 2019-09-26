1ecbb762fe71d44d98c30b2e1107865fb7e0e238
Karan Kumar <kumarkay.ca@gmail.com>
2019-09-25 20:10:48 -0400

    main modify

--- C0769647_MidTerm_F2019MAD3463/src/main/java/com/lambton/MainClass.java
+++ C0769647_MidTerm_F2019MAD3463/src/main/java/com/lambton/MainClass.java
@@ -9,10 +9,12 @@ public class MainClass {
         System.out.println(obj1.reverse(a));
 
         String name="KaDFran kmsr kUMadAR";
+
         System.out.println(obj1.initials(name));
 
         String word="karan kumar";
         System.out.println(obj1.mostFrequent(word));
+
         System.out.println(obj1.replaceSubString("my name is karan kumar ","His","my"));
 
     }
